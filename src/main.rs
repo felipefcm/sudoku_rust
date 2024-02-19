9c5f056ddc974b27aa29a92a554a61f6a735d2bf
Felipe Martins <ffcm@outlook.com>
2024-02-19 21:50:54 +1300

    improve imports in the main file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
-use sudoku_rs::loader;
-use sudoku_rs::solver::Solver;
-use sudoku_rs::sudoku::Sudoku;
+mod loader;
+mod solver;
+mod sudoku;
+
+use solver::Solver;
+use sudoku::Sudoku;
 
 fn main() {
     let puzzles = loader::load_file("puzzles.csv");
 
     for (i, puzzle) in (&puzzles[..10000]).iter().enumerate() {
-        println!("Solving Sudoku #{i}");
+        eprintln!("Solving Sudoku #{i}");
 
-        let mut state = Sudoku::parse_state(&puzzle.0);
+        let mut state = Sudoku::from_string(&puzzle.0);
 
         let solved = Solver::solve(&mut state);
         assert!(solved);
