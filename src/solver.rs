b439d79f6c0409af373d2935566bbb86fa4e328d
Felipe Martins <ffcm@outlook.com>
2024-02-19 21:51:17 +1300

    split the search function into two

--- src/solver.rs
+++ src/solver.rs
@@ -9,32 +9,33 @@ pub struct Solver {}
 impl Solver {
     pub fn solve(sudoku: &mut Sudoku) -> bool {
         let solver = Solver {};
+        let root_node = Node { index: 0 };
 
-        let root_node = Node {
-            index: 0, // find first empty position
-        };
-
-        solver.search(root_node, sudoku)
+        return solver.search(root_node, sudoku);
     }
 
     fn search(&self, node: Node, state: &mut Sudoku) -> bool {
-        let n = state.get_at_index(node.index);
-
         if state.is_index_fixed(node.index) {
-            if Sudoku::is_last_cell(node.index) {
-                return true;
-            }
-
-            let next_node = Node {
-                index: node.index + 1,
-            };
-
-            if self.search(next_node, state) {
-                return true;
-            };
-
-            return false;
+            return self.bypass_fixed(node, state);
         }
+
+        return self.make_attempts(node, state);
+    }
+
+    fn bypass_fixed(&self, node: Node, state: &mut Sudoku) -> bool {
+        if Sudoku::is_last_cell(node.index) {
+            return true;
+        }
+
+        let next_node = Node {
+            index: node.index + 1,
+        };
+
+        return self.search(next_node, state);
+    }
+
+    fn make_attempts(&self, node: Node, state: &mut Sudoku) -> bool {
+        let n = state.get_at_index(node.index);
 
         for i in n + 1..=9 {
             state.set_at_index(node.index, i);
@@ -56,10 +57,7 @@ impl Solver {
             };
         }
 
-        if !state.is_index_fixed(node.index) {
-            state.set_at_index(node.index, 0);
-        }
-
+        state.set_at_index(node.index, 0);
         return false;
     }
 }
